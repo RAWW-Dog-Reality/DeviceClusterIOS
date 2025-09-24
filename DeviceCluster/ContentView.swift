e0dc22359c8fc8cb4a59ea452a437c61a02dfc68
Daniyar <daniyar.kbv@gmail.com>
2025-09-24 13:39:23 +0500

    Initial Commit

---
+++ DeviceCluster/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DeviceCluster
+//
+//  Created by Daniyar Kurmanbayev on 2025-09-24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DeviceCluster/DeviceClusterApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeviceClusterApp.swift
+//  DeviceCluster
+//
+//  Created by Daniyar Kurmanbayev on 2025-09-24.
+//
+
+import SwiftUI
+
+@main
+struct DeviceClusterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
